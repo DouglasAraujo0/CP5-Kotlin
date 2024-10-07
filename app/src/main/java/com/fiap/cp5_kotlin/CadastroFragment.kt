281e9825ab7518970afc390341fe6d3d897a788e
Douglas Araujo <126580412+DouglasAraujo0@users.noreply.github.com>
2024-10-07 08:35:14 -0300

    Update CadastroFragment.kt

--- app/src/main/java/com/fiap/cp5_kotlin/CadastroFragment.kt
+++ app/src/main/java/com/fiap/cp5_kotlin/CadastroFragment.kt
@@ -18,22 +18,30 @@ class CadastroFragment : Fragment() {
     ): View {
         binding = FragmentCadastroBinding.inflate(inflater, container, false)
 
-        // Configurar o Spinner
-        val opcoes = arrayOf("Opção 1", "Opção 2", "Opção 3")  // Substitua com suas opções
+        // Configuração o Spinner
+        val opcoes = arrayOf("Opção 1", "Opção 2", "Opção 3")
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, opcoes)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerOpcao.adapter = adapter
 
+        // Definindo o texto do botão e os hints
+        binding.btnCadastrar.text = getString(R.string.cadastrar)  
+        binding.inputNome.hint = getString(R.string.nome)          
+        binding.inputEmail.hint = getString(R.string.email)       
+        binding.inputTelefone.hint = getString(R.string.telefone) 
+        binding.inputEndereco.hint = getString(R.string.endereco) 
+        binding.inputObservacao.hint = getString(R.string.observacao)
+
         binding.btnCadastrar.setOnClickListener {
-            // Coletar dados do formulário
+            // Coletando dados do formulário
             val nome = binding.inputNome.text.toString()
             val email = binding.inputEmail.text.toString()
             val telefone = binding.inputTelefone.text.toString()
             val endereco = binding.inputEndereco.text.toString()
             val observacao = binding.inputObservacao.text.toString()
-            val opcaoSelecionada = binding.spinnerOpcao.selectedItem.toString() // Obter a opção selecionada
+            val opcaoSelecionada = binding.spinnerOpcao.selectedItem.toString() 
 
-            // Repassar os dados para o fragmento de lista
+            // Repassando os dados para o fragmento de lista
             val listaFragment = ListaFragment()
             val bundle = Bundle().apply {
                 putString("NOME", nome)
@@ -41,10 +49,10 @@ class CadastroFragment : Fragment() {
                 putString("TELEFONE", telefone)
                 putString("ENDERECO", endereco)
                 putString("OBSERVACAO", observacao)
-                putString("OPCAO", opcaoSelecionada) // Passar a opção selecionada
+                putString("OPCAO", opcaoSelecionada)
             }
 
-            // Navegar para o fragmento de lista
+            // Navegando para o fragmento de lista
             findNavController().navigate(R.id.action_cadastroFragment_to_listaFragment, bundle)
         }
 
